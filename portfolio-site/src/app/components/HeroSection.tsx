d2e99c6b6dac55afe79a725457550c7b9400a0cd
Martin Milenkovski <martinmilex@gmial.com>
2025-02-17 17:04:15 +0100

    social links

--- portfolio-site/src/app/components/HeroSection.tsx
+++ portfolio-site/src/app/components/HeroSection.tsx
@@ -7,8 +7,39 @@ interface HeroProps
 const HeroSection: React.FC<HeroProps> = ({ name }) => {
   return (
     <div className=" justify-center items-center">
-      <h1 className="m-0 leading-none text-[320px] align-center">{name}</h1>
+      <div className="flex justify-center flex-col">
+        <h1 className="m-0 leading-none text-[330px] align-center inline">
+          {name}
+        </h1>
+      </div>
 
+      <div className="flex justify-center items-center gap-4">
+        <a
+          href=" " // github link  here
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-2"
+        >
+          <p>Git</p>
+        </a>
+        <a
+          href=" " // dribble link  here
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-2"
+        >
+          <p>Drb</p>
+        </a>
+
+        <a
+          href=" " // github link  here
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-2"
+        >
+          <p>Ig</p>
+        </a>
+      </div>
       <video
         src="https://videos.pexels.com/video-files/6986423/6986423-uhd_2732_1440_25fps.mp4"
         autoPlay
@@ -19,4 +50,4 @@ const HeroSection: React.FC<HeroProps> = ({ name }) => {
   );
 }
 
-export default HeroSection
+export default HeroSection;
