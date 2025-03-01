dbfe5c9e795bff6d793291d4eb069497efe5932b
martinmilenkvski <51443462+martinmilenkvski@users.noreply.github.com>
2025-03-01 18:46:14 +0100

    Nav hover

--- portfolio-site/src/app/components/NavBar.tsx
+++ portfolio-site/src/app/components/NavBar.tsx
@@ -13,17 +13,27 @@ const NavBar: React.FC<NavBarProps> = () => {
       className="flex p-4 w-full"
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
     >
       <ul className="flex gap-4 justify-between w-full">
-        {navItems.map((item, index) => (
+        {navItems.map((item) => (
           <motion.li
             key={item}
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: index * 0.1, duration: 0.5 }}
+            whileHover={{
+              opacity: 0.6,
+              transition: { duration: 0.1, ease: "easeInOut" },
+            }}
           >
-            {item === "ABOUT" ? <Link href="/about">{item}</Link> : item}
+            <Link href={`/${item.toLowerCase()}`} passHref>
+              <motion.a
+                whileHover={{
+                  opacity: 0.6,
+                  transition: { duration: 0.3, ease: "easeInOut" },
+                }}
+              >
+                {item}
+              </motion.a>
+            </Link>
           </motion.li>
         ))}
       </ul>
